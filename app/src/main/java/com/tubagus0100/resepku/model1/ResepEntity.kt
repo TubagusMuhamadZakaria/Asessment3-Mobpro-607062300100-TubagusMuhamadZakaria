92e67a841eb6ecabdc5fc2929d23489257821e05
TubagusMuhamadZakaria <tbmz999@gmail.com>
2025-05-08 17:16:49 +0700

    Menambahkan entitas Room lengkap untuk menyimpan data resep

---
+++ app/src/main/java/com/tubagus0100/resepku/model1/ResepEntity.kt
@@ -0,0 +1,14 @@
+package com.tubagus0100.resepku.model1
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "resep")
+data class ResepEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val judul: String,
+    val deskripsi: String,
+    val bahan: String,
+    val langkah: String
+)
